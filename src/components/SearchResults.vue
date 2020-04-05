52a53d00428173a7591123ff4230a913d8ca0d0c
Brock Wilcox <awwaiid@thelackthereof.org>
2020-04-05 00:40:39 -0400

    Remove dead code

--- src/components/SearchResults.vue
+++ src/components/SearchResults.vue
@@ -550,49 +550,6 @@ export default {
   },
   data: function() {
     return {
-      scores: {
-        score: [1, 2, 3, 4, 5],
-        riskCategory: ["Low", "Moderate", "Elevated", "High", "Critical"],
-        riskDescription: [
-          [
-            "•You can continue to do this activity.",
-            "•The frequency and nature of the activity puts you at low risk for contracting or transmitting COVID-19.",
-            "•You may still want to take extra precautions for prevent infection."
-          ],
-          [
-            "•You can continue to do this activity but you should consider ways to do this activity less or during less risky times.",
-            "•The frequency and nature of the activity puts you at some risk for contracting or transmitting COVID-19.",
-            "•Make sure to take extra precautions for prevent infection, like frequently washing your hands (with soap) for at least 20 seconds and remaining 6 feet from another person."
-          ],
-          [
-            "•You should consider stopping this activity.",
-            "•If you are unable to stop, you should consider ways to do this activity less or during less risky times.",
-            "•The frequency and nature of the activity puts you at more risk for contracting or transmitting COVID-19.",
-            "•Extra precautions to preventing infection are not enough, like frequently washing your hands (with soap) for at least 20 seconds and remaining 6 feet from another person. "
-          ],
-          [
-            "•You should stop this activity.",
-            "•The frequency and nature of the activity puts you at high risk for contracting or transmitting COVID-19.",
-            "•You may be endangering others and efforts to control the outbreak.",
-            "•Extra precautions to preventing infection are not enough, like frequently washing your hands (with soap) for at least 20 seconds and remaining 6 feet from another person. "
-          ],
-          [
-            "•Stop this activity immediately.",
-            "•The frequency and nature of the activity is endangering yourself and efforts to control the COVID-19 outbreak, regardless of extra precautions to preventing infection, like frequently washing your hands (with soap) for at least 20 seconds and remaining 6 feet from another person. "
-          ]
-        ]
-      },
-      ageGroups: {
-        groups: [
-          "Under 10",
-          "10 to 18",
-          "18 to 19",
-          "20 to 29",
-          "30 to 49",
-          "50 to 69",
-          "70+"
-        ]
-      },
       times: [
         "Right now",
         "1:00 AM",
