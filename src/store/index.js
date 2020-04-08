8f4c374fb6e203ee443b46c8f520a2e5018b4614
Brock Wilcox <awwaiid@thelackthereof.org>
2020-04-08 09:43:03 -0400

    Persist vuex state to localstorage to survive browser refresh

--- src/store/index.js
+++ src/store/index.js
@@ -1,9 +1,11 @@
 import Vue from "vue";
 import Vuex from "vuex";
+import VuexPersistence from "vuex-persist";
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
+  plugins: [new VuexPersistence().plugin],
   state: {
     profileCreated: false,
     submitted: false,
