b1fc6306a89bce7018a48850833d0794128119fb
dmkratochvil <44682959+dmkratochvil@users.noreply.github.com>
2020-04-09 19:58:24 -0400

    add privacy text

--- src/views/About.vue
+++ src/views/About.vue
@@ -11,7 +11,8 @@
         <tr>
           <td>The location and time that you do certain activities.</td>
           <td>
-            Whether it’s crowded at that time and location <br />Suggest
+            Whether it’s crowded at that time and location
+            <br />Suggest
             alternative times that reduce crowding risk.
           </td>
           <td>
@@ -21,17 +22,13 @@
           </td>
         </tr>
         <tr>
-          <td>
-            Whether you have been diagnosed with additional diseases/conditions.
-          </td>
+          <td>Whether you have been diagnosed with additional diseases/conditions.</td>
           <td>
             Whether your comorbidity increases your risk of developing severe
             illness.
-            <br />
-            Suggest that you take additional precautions, including avoiding
+            <br />Suggest that you take additional precautions, including avoiding
             public and more crowded places.
-            <br />
-            Suggest that you find alternative, lower risk ways of accomplishing
+            <br />Suggest that you find alternative, lower risk ways of accomplishing
             your daily tasks.
           </td>
           <td>
@@ -43,52 +40,226 @@
         </tr>
       </table>
     </div>
+<div style="margin:2em">
+    <div >
+      <h1>Who We Are</h1>
+      <table style="width:100%">
+        <tr>
+          <td>
+            <b>
+              <u>Risk Assessment</u>
+            </b>
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/in/jarjiehfang/">Jarjieh Fang, MPH</a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/gayatri-sanku-16212a4/">Gayatri Sanku, MPH</a>
+              </li>
+            </ul>
+          </td>
+          <td>
+            <b>
+              <u>Development</u>
+            </b>
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/in/brockwilcox/">Brock Wilcox, MS</a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/david-kratochvil-1bb61a11b/">David Kratochvil</a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/grant-hussey-a9854bb4/">Grant Hussey</a>
+              </li>
+            </ul>
+          </td>
+        </tr>
+        <tr>
+          <td>
+            <b>
+              <u>Public Relations</u>
+            </b>
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/in/andrewloeb/">Andrew Loeb</a>
+              </li>
+            </ul>
+          </td>
+          <td>
+            <b>
+              <u>Design</u>
+            </b>
+            <ul>
+              <li>
+                <a href="https://www.linkedin.com/in/meera-nathan-371b15132/">Meera Nathan</a>
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </table>
 
-    <br />
-    <b><u>Who Are We?</u></b>
-    <br />
-    <p align="left">
-      We are an independent and multi-disciplinary team. Our core development
-      team is comprised of:
-    </p>
-    <p></p>
-    <ul align="left">
-      <li>
-        Jarjieh Fang: Link to Bio
-        <a href="https://www.linkedin.com/in/jarjiehfang/">(LinkedIn)</a>
-      </li>
-      <li>
-        Gayatri Sanku: Link to Bio
-        <a href="https://www.linkedin.com/in/gayatri-sanku-16212a4/"
-          >(LinkedIn)</a
-        >
-      </li>
-      <li>
-        Grant Hussey: Link to Bio
-        <a href="https://www.linkedin.com/in/grant-hussey-a9854bb4/"
-          >(LinkedIn)</a
-        >
-      </li>
-      <li>
-        David Kratochvil: Link to Bio
-        <a href="https://www.linkedin.com/in/david-kratochvil-1bb61a11b/"
-          >(LinkedIn)</a
-        >
-      </li>
-      <li>
-        Andrew Loeb: Link to Bio
-        <a href="https://www.linkedin.com/in/andrewloeb/">(LinkedIn)</a>
-      </li>
-    </ul>
-    <br />
-    <p align="left">The following experts reviewed our tool:</p>
+      <h1>Who Our Advisors Are</h1>
+      <table style="width:100%">
+        <tr>
+          <td>
+            <b>
+              <u>Expert Reviewers</u>
+            </b>
+          </td>
+          <td>
+            <b>
+              <u>Clinicians</u>
+            </b>
+          </td>
+        </tr>
+        <tr style="vertical-align: top;">
+          <td>
+            <ul>
+              <li>
+                <a
+                  href="https://www.linkedin.com/in/saskia-popescu-phd-mph-ma-cic-4ba9a334/"
+                >Saskia Popescu, PhD, MPH, MA, CIC</a>
+              </li>
+              <li>
+                <a href="https://www.linkedin.com/in/yen-pottinger-8b82a992/">Yen Pottinger, PhD</a>
+              </li>
+            </ul>
+          </td>
+          <td>
+            <ul>
+              <li>
+                <a href="https://profiles.ucsf.edu/jayant.rajan">Jayant Rajan, M.D., P.h.D</a>
+              </li>
+              <li>
+                <a
+                  href="https://surgeryresidentresearch.ucsf.edu/research-fellows/general-surg
+ery/andrew-wisneski,-md.aspx"
+                ></a>Andrew Wisneski, M.D.
+              </li>
+              <li>Heide Kuang, M.D.</li>
+              <li>
+                <a
+                  href="https://depts.washington.edu/neurolog/education/residency/curr
+ent-residents/"
+                >Roger Chang, M.D., P.h.D.</a>
+              </li>
+              <li>
+                <a href="https://www.uclahealth.org/rheumatology/fellows">Ernest Maningding, M,D.</a>
+              </li>
+            </ul>
+          </td>
+        </tr>
+      </table>
+
+      <h1>Information Collection Statement (In the Brief)</h1>
+      <ol>
+        <li>
+          You should
+          <b>not</b> rely on us for medical advice. You should seek medical advice
+          from a licensed professional and follow the instructions of government
+          authorities.
+        </li>
+
+        <li>We will do our best to provide you with the best information available.</li>
+        <li>
+          If you notice something is incorrect or outdated, please
+          <router-link class="router" to="/contactUs">contact us.</router-link>
+        </li>
+        <li>
+          When using this service, you may provide additional details about
+          yourself.
+        </li>
+        <li>
+          The information provided is not collected or stored by
+          <i>COVID Can I Do It</i> in any way.
+        </li>
+        <li>
+          If you have any questions, please
+          <router-link class="router" to="/contactUs">contact us.</router-link>
+        </li>
+        <!-- need to set up this link once we have the full info collection statement -->
+        <li>
+          Read the full Information Collection Statement
+          <router-link class="router" to="/about">here</router-link>
+        </li>
+      </ol>
+    </div>
     <br />
     <p align="left">
       No one involved in the development of this tool has received any funding
       or profits for this project.
     </p>
-    <b><u>Your Privacy and Your Rights</u></b>
-    <b>[Andrew to fill out]</b>
+    <br />
+    <hr>
+    <br />
+    <div>
+      <!-- Privacy Policy -->
+        <!-- <b>
+          <u>Your Privacy and Your Rights</u>
+        </b> -->
+        
+        <h1>Information Collection Statement</h1>
+          <p>We make this question and answer service available to you as a convenience. We will do our best to provide you with the best information available and to keep information accurate and up-to-date, but the situation is changing rapidly, so we can’t guarantee this. We will date and provide a source for answers. If you notice something is incorrect or outdated, please contact us (info@covidcanidoit.com).  
+            You shouldn’t rely on us for medical advice. You should seek medical advice from a licensed professional and follow the instructions of government authorities. 
+            To provide this service, we work with a network of partners and volunteers, who may help us to process your request and answer your question.  If you email us with a suggested activity, our team may add the suggested activity to the site after review. When using this service, you may provide additional details about yourself. That information is not collected or stored by COVID Can I Do It in any way. 
+            Please do not provide us with any additional personally identifiable information beyond your name and email address.
+          <br/>
+          If you have any questions, please contact us at info@covidcanidoit.com. </p>
+          <br>
+          <p>
+            <b>Introduction to this Privacy Notice</b>
+            The COVID Can I Do It team ("we", "us", "our") cares about your privacy and wants you to be familiar with how we collect, disclose, and otherwise use ("Process") your information. This Information Collection Statement describes our practices in connection with the information we collect through this COVID Can I Do It  Site and Service ("Service"). Please read the following to learn more about our privacy practices.</p>
+          <p>Definition of Personal Information For purposes of this Information Collection Statement, "Personal Information" means any information that can be used to distinguish or trace an individual’s identity, either alone or when combined with other personal or identifying information that is linked or linkable to a specific individual.</p>
+          <p>What We Collect and How We Use Information In providing you the content on this Service, we may collect information from you when you voluntarily provide it to us, such as when you:  
+            <ul>
+              <li>Search for activities and answers</li>
+              <li>Submit a suggested activity to the Service</li>
+              <li>Share additional details about yourself to the service</li>
+              <li>Interact with the Service</li>
+            </ul>
+          </p>
+          <p>We use that information solely for the purposes of responding to your request and improving the Service. For example, we may share your suggested activity with a network of individuals who will research the answer to the question. We may also post a version of your question and our answer that would be useful to provide to the public via the Service. We will not collect your name or email address. We take reasonable steps to protect the information you provide us from loss, misuse and unauthorized access, disclosure, alteration, and destruction. We have implemented appropriate physical, electronic and managerial procedures to help safeguard and secure your information from loss, misuse, unauthorized access or disclosure, alteration or destruction. Unfortunately, no security system is 100% secure; thus, we cannot ensure the security of all information you provide to us via our services.  We work with partners to provide this service to you.</p>
+          <p>Other information we may collect about you:
+            <ul>
+              <li>Cookies: Our services may use “cookies” and similar technologies (collectively, "Cookies"). Cookies are small text files that this website sends to your computer for record-keeping purposes and this information is stored in a file on your computer’s hard drive. Cookies make web surfing and browsing easier for you by saving your preferences so that we can use these to improve your next visit to our website.
+                <ul>  
+                  <li>One of the purposes of cookies is to simplify the use of the website. A cookie may save a user’s login details for example, so that the user does not need to login every time.</li>
+                  <li>Cookies are also used for statistical purposes, to determine how the website is used.</li>
+                </ul>
+              </li>  
+              <li>Google Analytics: We may use Google Analytics to help us get a better understanding of how visitors use this website. The information generated by the Google Analytics cookie, about your use of this website, is transmitted to and stored by Google. If you do not want your activity on this website to be tracked by Google Analytics, you may opt out by using this link: http://tools.google.com/dlpage/gaoptout?hl=en.</li>
+              <li>Other Non-Personal Information: In addition to any personal information or other information that you choose to provide to us on our services, we and our third-party service providers may use a variety of technologies, now and hereafter devised, that automatically collect certain website usage information whenever you visit or interact with our services. This information may include browser type, operating system, the page served, the time, the source of the request, the preceding page view, and other similar information. We may use this usage information for a variety of purposes, including to enhance or otherwise improve our services. In addition, we may also collect your IP address or some other unique identifier for the particular device you use to access the Internet, as applicable (collectively, referred to herein as a "Device Identifier"). A device identifier is a number that is automatically assigned to your device, and we may identify your device by its device identifier. When analyzed, usage information helps us determine how our services are used, such as what types of visitors arrive at the services, what type of content is most popular, what type of content you may find most relevant and what type of visitors are interested in particular kinds of content and advertising. We may associate your device identifier or website usage information with the personal information you provide, but we will treat the combined information as personal information.</li>
+            </ul>
+          </p>
+          <p>Visitors agree not to provide Sensitive Personal Information via this Service. Sensitive Personal Information includes any of the following: (i) credit, debit or other payment card data subject to the Payment Card Industry Data Security Standards ("PCI DSS"); (ii) patient, medical or other protected health information regulated by the Health Insurance Portability and Accountability Act ("HIPAA"); or (iii) any other personal data of an EU citizen deemed to be in a "special category" (as identified in EU General Data Protection Regulation or any successor directive or regulation). You should not provide information that you do not wish to be made public. </p>
+          
+          <p>
+            <b>Our Service</b>
+            This Service provides information, translations and links to other websites as a convenience to you. These include links to websites operated by third-party government agencies, nonprofit organizations, universities and private businesses. Given the fast changing nature of the coronavirus crisis, we will do the best we can to keep information, translations and links up to date but please note that information, translations and links may become out-of-date and are not a substitute for official advice or medical advice from a licensed professional. The information that we provide you via the Service should be considered general information and not personal advice. When you use one of these links, you are no longer using this Service, and this Information Collection Statement does not apply. When you visit another website, you are subject to the privacy notice and information collection practices of that site. Any visitor who proceeds to those external sites and relies on any information obtained from them does so at his or her own risk and assumes any and all liability from damages which may result from accessing a third-party site linked to from the Service. We exercise no control over the third-party organization's views, accuracy, copyright or trademark compliance, or the legality of the material contained on those websites, and do not sponsor, endorse, or approve the information, content, products, materials, opinions or services contained on such external sites. Furthermore, when you follow a link to another website, portions of their information may be incorrect or out-of-date. COVID Can I Do It does not warrant the accuracy, reliability, or timeliness of any information published by external sites. </p>
+          <p>
+            <b>Access through Widget</b>
+            If you are accessing this service through a widget or site other than covidcanidoit.com, this Information Collection Statement only applies to content within the widget and interactions with the Service through the widget. The third-party site makes this widget available to you as a convenience and you are also subject to the terms, privacy notice and information collection practices of the host site for any interactions with the host site. The Service exercises no control over the third-party organization's views, accuracy, copyright or trademark compliance, or the legality of the material contained on those websites, and does not sponsor, endorse, or approve the information, content, products, materials, opinions or services contained on such sites.</p>
+          <p>
+            <b>Contact Information</b>
+            If after reviewing this Information Collection Statement, you have any questions, complaints or privacy concerns, or would like to make any requests in relation to your personal information, or obtain further information on safeguards, please send an email to info@covidcanidoit.com.</p>
+          <p>
+            <b>Copyright Notice Except</b>
+            where otherwise noted, content on this site is licensed under a Creative Commons Attribution 4.0 License. If you submit an activity to this Service whether by searching or contacting us, you license all rights to that submission for free worldwide duplication, modification, distribution and re-use.<p>
+          <p>
+            <b>DMCA Notice</b>
+            We respect the intellectual property of others, and we ask users of our Web sites to do the same. In accordance with the Digital Millennium Copyright Act (DMCA) and other applicable law, if you believe your copyright has been infringed please email us at: info@covidcanidoit.com.To be an effective notice under Section 512(c) of the DMCA your written notice must include all of the following:
+            <ul>
+              <li>A physical or electronic signature of a person authorized to act on behalf of the owner of an exclusive right that is allegedly infringed;</li>
+              <li>Identification of the copyrighted work claimed to have been infringed, or if multiple copyrighted works at a single site are covered by a single notification, a representative list of such works at that site;</li>
+              <li>Identification of the material that is claimed to be infringing or to be the subject of infringing activity and that is to be removed or access to which is to be disabled, and information reasonably sufficient to permit us to locate the material;</li>
+              <li>Information reasonably sufficient to permit us to contact you, such as an address, telephone number, and, if available, an e-mail address;</li>
+              <li>A statement that you have a good faith belief that use of the material in the manner complained of is not authorized by the copyright owner, its agent, or the law;</li>
+              <li>A statement that the information in the notification is accurate, and under penalty of perjury, that you are copyright owner or authorized to act on behalf of the owner of an exclusive right that is allegedly infringed.</li>
+            </ul>
+          </p>
+    </div>
+    </div>
   </div>
 </template>
 
