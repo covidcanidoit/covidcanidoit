6b496f862d2e8b3f516c52eaeba3b48a37ad9f4d
Brock Wilcox <awwaiid@thelackthereof.org>
2020-04-11 15:30:05 -0400

    Use the Roboto (free) font

--- src/App.vue
+++ src/App.vue
@@ -14,12 +14,14 @@
 </template>
 
 <style lang="scss">
+@import url('https://fonts.googleapis.com/css2?family=Roboto&display=swap');
+
 body {
   background-color: #ccc;
 }
 
 #app {
-  font-family: Geneva, Arial, sans-serif;
+  font-family: 'Roboto', sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
