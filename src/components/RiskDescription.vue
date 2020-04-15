d3f3db67b36389e99a3cd9004491075bd7d2f161
Johnny Li <reptile18@gmail.com>
2020-04-15 14:08:49 -0700

    remove inline references stuff

--- src/components/RiskDescription.vue
+++ src/components/RiskDescription.vue
@@ -16,12 +16,8 @@
       <Markdown class="risk-details" :source="risk && risk.longDescription" />
       <a href="#divMoreInfo" data-toggle="collapse" class="moreLessInfoLink" @click="hideMoreInfo=!hideMoreInfo">{{moreOrLessInfo}}</a>
       <div id="divMoreInfo" class="risk-references-container collapse">
-        <!-- uncomment this line for inline referenes -->
-        <!--<span>References: </span><span class="risk-reference">{{concatReferences}}</span>-->
-        <!-- uncomment this line for numbered list references -->
         <h5>Learn more: </h5>
         <ol>
-          <!--<li v-show="activity[reference] !== ''" v-for="(reference,index) in Object.keys(activity).filter((prop) => prop.indexOf('reference') > -1)" :key="index" class="risk-reference"> {{activity[reference]}}</li>-->
           <li v-for="(reference,index) in references" :key="index" class="risk-reference"> {{reference}}</li>
         </ol>
       </div>
@@ -48,20 +44,6 @@ export default {
     ...mapState(["riskLevels"]),
     risk() {
       return this.riskLevels[this.score - 1];
-    },
-    concatReferences: function() {
-      var referencesString = "";
-      var references = Object.keys(this.activity).filter((prop) => prop.indexOf("reference")>-1);
-      var reference;
-      var i;
-
-      for (i = 0; i < references.length; i++) {
-        reference = references[i];
-        if (this.activity[reference] === "") continue;
-        referencesString += this.activity[reference] + ", ";
-      }
-      referencesString = referencesString.substring(0,referencesString.length-2) + ".";
-      return referencesString;
     },
     references: function() {
       var referencePropertyNames = Object.keys(this.activity).filter((prop) => prop.indexOf("reference")>-1);
