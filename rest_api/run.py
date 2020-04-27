d3a3a770b08a76aecbada4b7eb2a7d839b6022bd
Brock Wilcox <awwaiid@thelackthereof.org>
2020-04-26 22:31:26 -0400

    Add docker and some setup

--- rest_api/run.py
+++ rest_api/run.py
@@ -3,4 +3,4 @@ from flaskapp import start_flask_server
 flask_server = start_flask_server()
 
 if __name__ == "__main__":
-    flask_server.run(debug=True)
+    flask_server.run(debug=True,host='0.0.0.0')
