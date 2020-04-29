d812f1a680dc27a5c3a6c9602ae484304a487796
Johnny Li <reptile18@gmail.com>
2020-04-28 17:39:39 -0700

    functional tests

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,8 +1,21 @@
 // https://docs.cypress.io/api/introduction/api.html
 
-describe("My First Test", () => {
-  it("Visits the app root url", () => {
+describe("Homepage", () => {
+  it("Loads with header text", () => {
     cy.visit("/");
-    cy.contains("h1", "Welcome to Your Vue.js App");
+    cy.contains(".header", "I wonder if it's safe to");
+  });
+  it("Shows the nav", () => {
+    cy.visit("/");
+    cy.contains(".navigation", "Find An Activity");
   });
 });
+
+describe("Browsing Categories", () => {
+  it("Navigates from homepage to categories", () => {
+    cy.visit("/");
+    cy.contains("Find An Activity").click();
+    cy.contains("h1","Browse activities by category");
+  });
+
+})
