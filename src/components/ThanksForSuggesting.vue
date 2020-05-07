6792c9e8a6a91c87b0a1b8246e07ecb77aa5d9f2
Johnny Li <reptile18@gmail.com>
2020-05-07 15:03:10 -0700

    basic styling for no thanks for suggesting component

--- src/components/ThanksForSuggesting.vue
+++ src/components/ThanksForSuggesting.vue
@@ -1,7 +1,7 @@
 <template>
   <div v-show="NoResultsToShow">
-    <div class="container-sm text-center m-5">
-      <span>Thanks for suggesting this activity!</span>
+    <div class="container-sm text-center p-5 colorBlock">
+      <h2>Thanks for suggesting this activity!</h2>
     </div>
   </div>
 </template>
@@ -13,5 +13,7 @@ export default {
 </script>
 
 <style scoped>
-
+.colorBlock {
+  background-color: #8ad4b4;
+}
 </style>
