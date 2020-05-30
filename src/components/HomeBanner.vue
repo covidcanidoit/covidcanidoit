4a015e826438705469e164d4cc267591f5975c3d
Johnny Li <reptile18@gmail.com>
2020-05-30 12:58:33 -0700

    [#58] add banner for protests

---
+++ src/components/HomeBanner.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <a href="./PROTEST_V2.jpg">
+  <v-alert 
+  class="banner" 
+  color="#fd6167" 
+  type="warning" 
+  icon=" "
+  >
+    Going to a protest?<br>
+    Here are some tips >>>
+  </v-alert>
+  </a>
+  </div>
+</template>
+
+<script>
+export default {
+  
+}
+</script>
+
+<style scoped lang="scss">
+.banner {
+  font-size: 2em;
+  text-align: center;
+}
+</style>
