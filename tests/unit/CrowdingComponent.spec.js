c8179fd465094069a7209594e01b39a2fc66ad04
Jennifer Tran <jet054@eng.ucsd.edu>
2020-06-15 14:56:58 -0700

    Successfully mocked document, yay

--- tests/unit/CrowdingComponent.spec.js
+++ tests/unit/CrowdingComponent.spec.js
@@ -3,6 +3,9 @@ import CrowdingComponent from "@/components/CrowdingComponent.vue";
 
 describe("CrowdingComponent", () => {
   describe("Methods", () => {
+    const documentSpy = jest.spyOn(document, "getElementById");
+    const consoleSpy = jest.spyOn(console, "log");
+
     test("searchLocation should return the search location and set haveSearched to true", () => {
       const data = {
         haveSearched: false,
@@ -14,21 +17,44 @@ describe("CrowdingComponent", () => {
       const wrapper = shallowMount(CrowdingComponent, {
         propsData: data
       });
+
+      documentSpy.mockReturnValueOnce("something");
+      wrapper.vm.searchLocation();
       expect(data.selectedLocation).toMatchInlineSnapshot(
         `"391 Amsterdam Ave"`
       );
+      expect(consoleSpy).toBeCalled();
+      expect(consoleSpy).toMatchInlineSnapshot(`
+        [MockFunction] {
+          "calls": Array [
+            Array [
+              "searched: ",
+              undefined,
+              " at ",
+              "1:00 AM",
+            ],
+          ],
+          "results": Array [
+            Object {
+              "type": "return",
+              "value": undefined,
+            },
+          ],
+        }
+      `);
+      // expect(data.haveSearched).toReturnWith(true);
     });
   });
 
   test("returns correctly", () => {
     const wrapper = shallowMount(CrowdingComponent, {
-      props: { busyResults: "", hasSearched: false }
+      propsData: { busyResults: new Object(), hasSearched: false }
     });
 
     expect(wrapper.html()).toMatchInlineSnapshot(`
       <div class="crowding-data">
         <div style="display: none;">
-          <chart-stub style="display: none;"></chart-stub>
+          <chart-stub crowdingdata="[object Object]" style="display: none;"></chart-stub>
         </div>
       </div>
     `);
