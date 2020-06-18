95316d46c9ceff3cab61df55643f4c1027112cfc
Jennifer Tran <jet054@eng.ucsd.edu>
2020-06-17 21:25:10 -0700

    lint worflow

--- jest.config.js
+++ jest.config.js
@@ -2,5 +2,5 @@ module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
   verbose: true,
   moduleFileExtensions: ["js", "json", "vue"],
-  collectCoverageFrom: ["src/components/*.spec.{js,vue}", "!**/node_modules/**"]
+   collectCoverageFrom: ["src/components/*.spec.{js,vue}", "!**/node_modules/**"]
 };
