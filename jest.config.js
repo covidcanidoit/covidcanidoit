34f9831df14ade19083a8116e482fd4d9006d0c8
Jennifer Tran <jet054@eng.ucsd.edu>
2020-06-18 15:10:15 -0700

    testing commit time lint

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   preset: "@vue/cli-plugin-unit-jest",
   verbose: true,
-    moduleFileExtensions: ["js", "json", "vue"],
-   collectCoverageFrom: ["src/components/*.spec.{js,vue}", "!**/node_modules/**"]
+ moduleFileExtensions: ["js", "json", "vue"],
+  collectCoverageFrom: ["src/components/*.spec.{js,vue}", "!**/node_modules/**"]
 };
