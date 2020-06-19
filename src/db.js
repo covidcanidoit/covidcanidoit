c1f6f1bf2ec0bda249db58bb3f58ab602e09cbc4
Brock Wilcox <awwaiid@thelackthereof.org>
2020-06-19 18:11:29 -0400

    Use the staging database by default

--- src/db.js
+++ src/db.js
@@ -3,14 +3,14 @@ import "firebase/database";
 import "firebase/auth";
 
 firebase.initializeApp({
-  apiKey: "AIzaSyCi3kGBTNKlQ68Tthd0qd1UbXmdg3yedCM",
-  authDomain: "coronavirus-6b732.firebaseapp.com",
-  databaseURL: "https://coronavirus-6b732.firebaseio.com",
-  projectId: "coronavirus-6b732",
-  storageBucket: "coronavirus-6b732.appspot.com",
-  messagingSenderId: "1001077196278",
-  appId: "1:1001077196278:web:f81c37955ece1804a6b3d7",
-  measurementId: "G-PGWKK9FXZV"
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.VUE_APP_FIREBASE_APP_ID,
+  measurementId: process.env.VUE_APP_FIREBASE_MEASUREMENT_ID
 });
 
 const db = firebase.database();
