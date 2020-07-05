9cf6369f4ef014559ed4a042242d7a782d379379
Brock Wilcox <awwaiid@thelackthereof.org>
2020-07-04 22:58:43 -0400

    Restore profile select dropdown styles [#129]

--- src/components/MultiQuestion.vue
+++ src/components/MultiQuestion.vue
@@ -42,4 +42,37 @@ export default {
     font-size: 1em;
   }
 }
+
+.form-select {
+  /* display: inline-block;
+  max-width: 100%;
+  height: 34px;
+  padding-right: 24px; */
+  /* background-color: rgb(186, 214, 26); */
+  /* background-repeat: no-repeat;
+  background-position: right 8px center;
+  background-size: 8px 10px; */
+  -webkit-appearance: none;
+  -moz-appearance: none;
+  appearance: none;
+  background: url(https://cdn1.iconfinder.com/data/icons/cc_mono_icon_set/blacks/16x16/br_down.png)
+    no-repeat right 16px center #ddd;
+
+  min-height: 34px;
+  min-width: 120px;
+  padding: 6px 8px;
+  font-size: 16px;
+  line-height: 20px;
+  /* color: #24292e; */
+  vertical-align: middle;
+  /* background-color: rgb(247, 12, 12); */
+  /* background-repeat: no-repeat;
+  background-position: right 8px center; */
+  /* border: 1px solid #d1d5da; */
+  border-radius: 30px;
+  outline: none;
+  box-shadow: inset 0 1px 2px rgba(27, 31, 35, 0.075);
+}
+
+
 </style>
