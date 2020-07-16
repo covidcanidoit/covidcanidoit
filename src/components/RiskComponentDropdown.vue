6bce5796ec9e80f6a8c786e0d1bd86ce137b4835
Johnny Li <reptile18@gmail.com>
2020-07-16 15:55:52 -0700

    adding risk component dropdowns for mobile [#166]

--- src/components/RiskComponentDropdown.vue
+++ src/components/RiskComponentDropdown.vue
@@ -1,20 +1,21 @@
 <template>
-  <v-row class="risk-component" justify="center" :class="riskClass">
-    <v-col cols="2" md="12">
-      <CrowdingIcon v-if="isCrowding" class="componentIcon" />
-      <DropletsIcon v-if="isDroplets" class="componentIcon" />
-      <TimeIcon v-if="isTime" class="componentIcon" />
-      <VentIcon v-if="isVent" class="componentIcon" />
-    </v-col>
-    <v-col cols="6" md="12">
-      <div class="componentTitle">{{ title }}</div>
-      <div class="componentRiskLabel">{{ risk }}</div>
-      <div class="notes">
-        <Markdown :source="notes" />
-        <!-- <v-btn>Learn more about {{ type }}</v-btn> -->
+  <v-expansion-panel class="riskComponentDropdown">
+    <v-expansion-panel-header class="d-flex flex-row">
+      <div :class="riskClass" class="componentIconContainer" flex-shrink-1>
+        <CrowdingIcon v-if="isCrowding" class="componentIcon" />
+        <DropletsIcon v-if="isDroplets" class="componentIcon" />
+        <TimeIcon v-if="isTime" class="componentIcon" />
+        <VentIcon v-if="isVent" class="componentIcon" />
       </div>
-    </v-col>
-  </v-row>
+      <div class="componentHeaderText" flex-grow-1>
+        <div class="componentTitle">{{ title }}</div>
+        <div class="componentRiskLabel">{{ risk }}</div>
+      </div>
+    </v-expansion-panel-header>
+    <v-expansion-panel-content>
+      <Markdown :source="notes" />
+    </v-expansion-panel-content>
+  </v-expansion-panel>
 </template>
 
 <script>
@@ -87,12 +88,28 @@ div .cls-2 {
 .riskHigh * {
   fill: $stopred;
 }
+.componentHeaderText {
+  font-size: 2em;
+  text-align: left;
+}
 .componentTitle {
-  font-weight: bold;
+  font-weight: 900;
+  margin-bottom: 0.25em;
+}
+.componentRiskLabel {
+  font-weight: 100;
+  color: $color-medgrey;
 }
 .notes {
   margin: 1em;
   text-align: left;
   font-size: 0.75em;
+}
+.riskComponentDropdown:nth-child(n + 2) {
+  border-top: 1px solid $color-lightgrey;
+}
+.componentIconContainer {
+  flex: 1 1 75 !important;
+  padding-right: 5em;
 }
 </style>
