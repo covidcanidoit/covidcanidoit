9d16ebedd9e66c792f09387cf22ff49811d2c741
Srilekha Nuli <srilekhanuli@gmail.com>
2020-07-25 14:08:16 -0400

    [#189]: update score scale design

--- tests/unit/ScoreScale.spec.js
+++ tests/unit/ScoreScale.spec.js
@@ -2,185 +2,26 @@ import { shallowMount } from "@vue/test-utils";
 import ScoreScale from "@/components/ScoreScale.vue";
 
 describe("ScoreScale", () => {
-  test("returns with class `connector`", () => {
+  test("all score entries have class 'score-scale-entry'", () => {
     const wrapper = shallowMount(ScoreScale);
-    expect(wrapper.findAll(".connector").length).toEqual(4);
+    expect(wrapper.findAll(".score-scale__entry").length).toEqual(5);
   });
 
-  test("returns with class `score-scale-entry", () => {
+  test("if no score is provided, no score will be selected", () => {
     const wrapper = shallowMount(ScoreScale);
-    expect(wrapper.findAll(".score-scale-entry").length).toEqual(5);
+    expect(wrapper.find(".selected").exists()).toBeFalsy();
   });
 
-  test("returns correctly without set score", () => {
-    const wrapper = shallowMount(ScoreScale);
-    expect(wrapper.html()).toMatchInlineSnapshot(`
-      <div class="score-scale">
-        <div class="score-scale-entry">
-          1
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          2
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          3
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          4
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          5
-        </div>
-      </div>
-    `);
-  });
+  test.each([[1], [2], [3], [4], [5]])(
+    "returns correctly with score set to 1",
+    score => {
+      const wrapper = shallowMount(ScoreScale, {
+        propsData: { score }
+      });
 
-  test("returns correctly with score set to 1", () => {
-    const wrapper = shallowMount(ScoreScale, {
-      propsData: { score: 1 }
-    });
-    expect(wrapper.html()).toMatchInlineSnapshot(`
-      <div class="score-scale">
-        <div class="score-scale-entry selected selected-1">
-          1
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          2
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          3
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          4
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          5
-        </div>
-      </div>
-    `);
-  });
-
-  test("returns correctly with score set to 2", () => {
-    const wrapper = shallowMount(ScoreScale, {
-      propsData: { score: 2 }
-    });
-    expect(wrapper.html()).toMatchInlineSnapshot(`
-      <div class="score-scale">
-        <div class="score-scale-entry">
-          1
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry selected selected-2">
-          2
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          3
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          4
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          5
-        </div>
-      </div>
-    `);
-  });
-
-  test("returns correctly with score set to 3", () => {
-    const wrapper = shallowMount(ScoreScale, {
-      propsData: { score: 3 }
-    });
-    expect(wrapper.html()).toMatchInlineSnapshot(`
-      <div class="score-scale">
-        <div class="score-scale-entry">
-          1
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          2
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry selected selected-3">
-          3
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          4
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          5
-        </div>
-      </div>
-    `);
-  });
-
-  test("returns correctly with score set to 4", () => {
-    const wrapper = shallowMount(ScoreScale, {
-      propsData: { score: 4 }
-    });
-    expect(wrapper.html()).toMatchInlineSnapshot(`
-      <div class="score-scale">
-        <div class="score-scale-entry">
-          1
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          2
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          3
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry selected selected-4">
-          4
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          5
-        </div>
-      </div>
-    `);
-  });
-
-  test("returns correctly with score set to 5", () => {
-    const wrapper = shallowMount(ScoreScale, {
-      propsData: { score: 5 }
-    });
-    expect(wrapper.html()).toMatchInlineSnapshot(`
-      <div class="score-scale">
-        <div class="score-scale-entry">
-          1
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          2
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          3
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry">
-          4
-        </div>
-        <div class="connector"></div>
-        <div class="score-scale-entry selected selected-5">
-          5
-        </div>
-      </div>
-    `);
-  });
+      const selectedScores = wrapper.findAll(".selected");
+      expect(selectedScores).toHaveLength(1);
+      expect(selectedScores.at(0).classes()).toContain(`score-${score}`);
+    }
+  );
 });
