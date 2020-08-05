093d49dc923de8daee30dd856e748a389f9b8892
Jennifer Tran <jet054@eng.ucsd.edu>
2020-08-05 12:41:14 -0700

    Add padding and make container fluid

--- src/views/About.vue
+++ src/views/About.vue
@@ -193,7 +193,7 @@
         <br />
         <hr style="border: 1px solid; color: #009898;" />
         <br />
-        <v-container style="align:left">
+        <v-container fluid class="pa-2 ma-0">
           <v-row>
             <v-col cols="12" md="4">
               <v-card class="pa-0" outlined tile>
