569200288c445ba15403055d88e597c8d4710454
Jennifer Tran <jet054@eng.ucsd.edu>
2020-08-27 16:41:01 -0700

    Fix typo [#252]

--- src/components/HomeBanner.vue
+++ src/components/HomeBanner.vue
@@ -5,7 +5,7 @@
       target="_blank"
     >
       <v-alert class="banner" color="$secondary">
-        Looking for hurricane or evacuation resources? We’ve gathered a fews
+        Looking for hurricane or evacuation resources? We’ve gathered a few >>>
         &gt;&gt;&gt;
       </v-alert>
     </a>
