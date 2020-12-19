9e6292cdc9597de6a62451ce1e7aa794810041f8
Johnny Li <reptile18@gmail.com>
2020-12-18 17:41:11 -0800

    remove debug flag from functions and delete helloWorld function

--- functions/index.js
+++ functions/index.js
@@ -7,10 +7,6 @@ const csv = require("csvtojson");
 
 // // Create and Deploy Your First Cloud Functions
 // // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-exports.helloWorld = functions.https.onRequest((request, response) => {
-  response.send("Hello from Firebase!");
-});
 
 const abbrv = {
   Arizona: "AZ",
